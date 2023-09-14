2f6551d9d9d9fd3c02079b2cd1f25450bdd06086
Francisco <mau910462@gmail.com>
2023-09-14 13:18:35 -0600

    Mi primer commit

--- WinUI/Program.cs
+++ WinUI/Program.cs
@@ -17,6 +17,7 @@ namespace WinUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("sdf");
         }
     }
 }
